f795782f75549a3cbc7d5630d6933c570458aff5
Tamy <tamy8677@gmail.com>
2024-09-17 17:51:02 +0800

    string

--- main.py
+++ main.py
@@ -37,3 +37,49 @@ name = True
 print(name)
 # 一個變數被修改（更新）後，後面的更動 不會 影響 前面的更動
 # swift中的變數 變數名稱 宣告時前面要加 var，python則是直接打變數名稱
+
+# 如何使用字串、字串的用法
+# 換行
+print("Hello \nMr.White")
+
+# Hello" Mr.White
+print("Hello\" Mr.White")
+# \告訴程式說 引號 是字串的一部分
+
+# 字串的相連
+print("Hello" + "Mr.White")
+
+# 變數
+phrase = "Hello"
+print(phrase + "Mr.white")
+
+# 字串函式 function（資料運算處理，回傳資料）
+# .lower() 變成小寫
+# .upper() 變成大寫
+# .islower() 檢查是否為小寫
+# .isupper() 檢查是否為大寫
+phrase = "Hello Mr.White"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.islower())
+print(phrase.isupper())
+# 函式疊加
+print(phrase.lower().islower())
+
+# Hello Mr.White
+# 012345678
+# 用位置找字母
+print(phrase[1])
+print(phrase[0])
+print(phrase[6])
+print(phrase[5])
+# .index("") 找字的位置
+print(phrase.index("H"))
+print(phrase.index("M"))
+print(phrase.index("l"))
+# 若有好幾個一樣的字，會回傳第一個的位置
+
+# .replace("被替代之字串","替代後之字串") 替代函式
+print(phrase.replace("H","h"))
+print(phrase.replace("l","L"))
+
