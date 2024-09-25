8723e60c9db9b682e0ccf211b928c3ec3777e803
Tamy <tamy8677@gmail.com>
2024-09-25 21:02:21 +0800

    advance caculate

--- main.py
+++ main.py
@@ -517,3 +517,49 @@ def max_num(num1,num2,num3):
         return num3
 print(max_num(2,3,5))
 # -->5
+
+# 建立一個計算機
+num1=float(input("請輸入第一個數： "))
+operator = input("請輸入運算符號： ")
+num2=float(input("請輸入第二個數： "))
+
+if operator=="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="/":
+    print(num1/num2)
+else:
+    print("不支援的運算")
+
+# -->
+# 請輸入第一個數： 5    
+# 請輸入運算符號： -
+# 請輸入第二個數： 2.2
+# 2.8
+
+# -->
+# 請輸入第一個數： 5
+# 請輸入運算符號： *
+# 請輸入第二個數： 2.2
+# 11.0
+
+# -->
+# 請輸入第一個數： 5
+# 請輸入運算符號： *
+# 請輸入第二個數： 2.2
+# 11.0
+
+# -->
+# 請輸入第一個數： 5
+# 請輸入運算符號： /
+# 請輸入第二個數： 2.5
+# 2.0
+
+# -->
+# 請輸入第一個數： 5
+# 請輸入運算符號： ＃
+# 請輸入第二個數： 2.2
+# 不支援的運算
