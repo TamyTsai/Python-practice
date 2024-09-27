bd37dfafc818db74db8a141c3a566f565e0e6369
TamyTsai <97825677+TamyTsai@users.noreply.github.com>
2024-09-27 20:43:57 +0800

    while

--- main.py
+++ main.py
@@ -589,3 +589,19 @@ print(dic[1])
 # {0: 'apple', 1: 'banana', 2: 'cat', 3: 'dog'}
 # apple
 # banana
+
+# while 迴圈（while迴圈與if陳述句類似，其檢查條件是否為真，並在條件保持正確狀態下，重複執行代碼塊）
+
+i=1
+while i<=5:
+    print(i)
+    i += 1
+    #相當於 i=i+1 也就是i會一直增加1
+print("迴圈結束")
+# -->
+# 1
+# 2
+# 3
+# 4
+# 5
+# 迴圈結束
