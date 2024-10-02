02103e64eadf648c82f61f5dcee762afc185680d
TamyTsai <97825677+TamyTsai@users.noreply.github.com>
2024-10-02 21:04:00 +0800

    bmi calculator

--- main.py
+++ main.py
@@ -755,3 +755,17 @@ def power(base_num,pow_num):
 print(power(2,5))
 # -->
 # 32
+
+# bmi計算器與錯誤處理
+try:
+  height = int(input("請輸入你的身高（公分）"))
+  weight = int(input("請輸入你的體重（公斤）"))
+  bmi = weight/(height/100)**2
+except ZeroDivisionError as err: # 分母為零的錯誤處理
+  print(f"身高不得為0，錯誤原因：{err}") # 用f搭配大括弧，來處理變數與字串的串接
+except ValueError as err: # 數值錯誤的錯誤處理
+  print("不得輸入非數字，錯誤原因",err) # 用課程講義中逗號的方式，來處理變數與字串的串接
+except: # 通用錯誤處理，用來捕捉漏網之魚
+  print("輸入有誤")
+else: # 沒出錯的時候
+  print(f"你的BMI是{bmi}")
